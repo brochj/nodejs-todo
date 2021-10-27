0cf0c7cc3b5da6d0536f5ecf662e43fa2529cdc6
brochj <brochj@gmail.com>
2021-10-27 11:47:35 -0300

    run eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 server.use(express.json());
 
@@ -16,41 +16,41 @@ server.use((req, res, next) => {
 // middleware local
 function checkProjectIdExists(req, res, next) {
   const { id } = req.params;
-  const project = projects.find(project => project.id == id);
+  const project = projects.find((project) => project.id == id);
 
   if (!project) {
-    return res.status(400).json({ error: 'Project not found' });
+    return res.status(400).json({ error: "Project not found" });
   }
 
-  req.index = projects.findIndex(project => project.id == id);
+  req.index = projects.findIndex((project) => project.id == id);
   return next();
 }
 
 // Adicionar Projeto
-server.post('/projects', (req, res) => {
+server.post("/projects", (req, res) => {
   const { id, title, tasks } = req.body;
 
   projects.push({ id, title, tasks });
 
-  return res.json({ id, title, tasks })
+  return res.json({ id, title, tasks });
 });
 
 // Adicionar Tarefa em um projeto
-server.post('/projects/:id/tasks', checkProjectIdExists, (req, res) => {
+server.post("/projects/:id/tasks", checkProjectIdExists, (req, res) => {
   const { title } = req.body;
 
   projects[req.index].tasks.push(title);
 
-  return res.json(projects[req.index])
+  return res.json(projects[req.index]);
 });
 
 // Listar todos os Projetos
-server.get('/projects', (req, res) => {
+server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
 // Alterar o titulo dos projetos
-server.put('/projects/:id', checkProjectIdExists, (req, res) => {
+server.put("/projects/:id", checkProjectIdExists, (req, res) => {
   const { title } = req.body;
 
   projects[req.index] = { ...projects[req.index], title };
@@ -59,8 +59,7 @@ server.put('/projects/:id', checkProjectIdExists, (req, res) => {
 });
 
 // Deletar um projeto
-server.delete('/projects/:id', checkProjectIdExists, (req, res) => {
-
+server.delete("/projects/:id", checkProjectIdExists, (req, res) => {
   projects.splice(req.index, 1);
 
   return res.json({ msg: `Project removed` });
